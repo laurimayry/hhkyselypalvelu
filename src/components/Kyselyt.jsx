f88446fa772dad3640e8b7e7e7f45ce32fcdf20d
Lauri Mäyry <lauri.mayry@gmail.com>
2023-11-22 13:54:25 +0200

    fetch by id

--- src/components/Kyselyt.jsx
+++ src/components/Kyselyt.jsx
@@ -4,43 +4,42 @@ function Kyselyt() {
   const backendUrl = "http://localhost:8080/kyselytREST";
   const [kysely, setKysely] = useState([]);
 
-
+  // eslint-disable-next-line no-unused-vars
+  const [kyselyId, setKyselyId] = useState(2);
 
   useEffect(() => {
-    //window.location.href = backendUrl;
+    const fetchKyselyt = () => {
+      const url = `${backendUrl}/${kyselyId}`;
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Virhe tietojen hakemisessa");
+          }
+        })
+        .then(data => {
+          console.log("Saatu data:", data); // Lisää tämä rivi nähdäksesi saadun datan
+
+          if (typeof data === 'object' && data !== null) {
+            setKysely([data]); // Huomaa, että käytetään silti taulukkoa käsittelyyn
+          } else {
+            throw new Error("Haettu data ei ole odotettu muoto");
+          }
+        })
+        .catch(error => {
+          console.error("Virhe:", error);
+        });
+    };
+
     fetchKyselyt();
-  }, []);
-
-  
-
-  const fetchKyselyt = () => {
-    fetch(backendUrl)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Virhe tietojen hakemisessa");
-      }
-    })
-    .then(data => {
-      if (Array.isArray(data)) {
-        setKysely(data);
-        console.log(data)
-
-      } else {
-        throw new Error("Haettu data ei ole taulukko");
-      }
-  })
-    .catch(error => {
-      console.error("Virhe:", error);
-  })}
+  }, [kyselyId, backendUrl]); // Lisää fetchKyselyt ja backendUrl riippuvuuslistaan
 
   return (
     <div>
       {kysely.map((kysely, index) => (
         <div key={index}>
-          {kysely.kyselyId},{" "}
-          {kysely.name}
+          {kysely.kyselyId}, {kysely.name}
         </div>
       ))}
     </div>
